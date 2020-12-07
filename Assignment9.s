2c749253cb97793368e9c1c0acf9f1d61b6409a6
troy bechtold <troy.bechtold@gmail.com>
2020-12-07 14:49:27 -0800

    final

--- Assignment9.s
+++ Assignment9.s
@@ -15,15 +15,15 @@
         .text
 _start:
         # write your program below
+        #prepare arguments in registers %rdi, %rsi, %rdx 
+        push %rbx
         movq $10, %rdi #x 
         movq $5, %rsi #y
         movq $1, %rdx #z
-	#prepare arguments in registers %rdi, %rsi, %rdx 
-	callq FindMax
-        # callq FindMax
-        # copy the return value from subroutine
-
-		# write your program above
+	callq FindMax # callq FindMax
+	movq %rax, %rbx # copy the return value from subroutine
+	pop %rbx
+	# write your program above
 
         # exit(0)
         mov     $60, %rax               # system call 60 is exit
@@ -33,24 +33,23 @@ _start:
 FindMax: 
         # write your code below 
 	#_start:
-        # write your program below
         movq %rdi, %rcx #x 
-        movq %rsi, %rbx #y
-        #movq %rdx, %rdx #z
-        cmpq % rcx, %rbx #x < y
+        movq %rsi, %rbp #y
+        movq %rdx, %r8 #z
+        cmpq % rcx, %rbp #x < y
         jle _xlarge #x > y
         jmp _ylarge #x < y
 
  _xlarge:
-        cmpq %rcx, %rdx #compare x and z
+        cmpq %rcx, %r8 #compare x and z
         jle _xlargest #x > z
-        movq % rdx, %rax #x < z
+        movq % r8, %rax #x < z
         jmp _end 
  
 _ylarge:
-        cmpq %rbx, %rdx #compare x and z
+        cmpq %rbp, %r8 #compare x and z
         jle _ylargest #y > z
-        movq %rdx, %rax #y < z
+        movq %r8, %rax #y < z
         jmp _end 
   
 _xlargest:
@@ -58,14 +57,11 @@ _xlargest:
         jmp _end 
  
 _ylargest:
-        movq %rbx, %rax 
+        movq %rbp, %rax 
         jmp _end 
  
 _end:
-        ret
-#end of program
-        #before return to caller, make sure the return value is in %rax
-        #inside the subroutine, donâ€™t jump to exit(0) since it terminates your program. Jump to â€œretqâ€ instead, because when the subroutine completes, it needs go back to caller. You will need put a label besides the instruction â€œretqâ€
-        #Done: retq
-		
+        Return:
+        retq #Done: retq
+        #end of program
         # write your code above
